7798dd873754b54df57f803667473f85b570115e
Shivang Mangal <shivangmangal@gmail.com>
2022-10-17 14:12:02 +0530

    dsa/dp/FibanociNumber.cpp ADDED

---
+++ dsa/dp/FibanociNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1)
+            return n;
+        int F[n+1];
+        F[0]=0;F[1]=1;
+        for(int i=2;i<=n;i++)
+            F[i] = F[i-1]+F[i-2];
+        return F[n];
+    }
+};
