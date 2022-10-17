2630d9864cff0a314cbe066ae1b73d32d806b226
Shivang Mangal <shivangmangal@gmail.com>
2022-10-17 14:12:09 +0530

    dsa/dp/nthTribonaci.cpp ADDED

---
+++ dsa/dp/nthTribonaci.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n<=1)
+            return n;
+        if(n==2)
+            return 1;
+        int F[n+1];
+        F[0] = 0;F[1]=1;F[2]=1;
+        for(int i=3;i<=n;i++)
+            F[i] = F[i-1]+F[i-2]+F[i-3];
+        return F[n];
+    }
+};
