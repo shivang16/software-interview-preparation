d9ed4b471efe4ff73d0bed2fcacbc2f2b27f5100
Shivang Mangal <shivangmangal@gmail.com>
2022-10-27 00:49:56 +0530

    dsa/random/findDupilcateInArray.cpp ADDED

---
+++ dsa/random/findDupilcateInArray.cpp
@@ -0,0 +1,15 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+    int slow = A[0];
+    int fast = A[0];
+    do{
+        slow = A[slow];
+        fast = A[A[fast]];
+    }while(slow!=fast);
+    fast = A[0];
+    while(slow!=fast){
+        slow = A[slow];
+        fast = A[fast];
+    }
+    return slow;
+}
+
