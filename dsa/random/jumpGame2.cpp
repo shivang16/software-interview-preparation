9806ce0c3eafd0bfb0ad29b6820845fcfb4205e4
Shivang Mangal <shivangmangal@gmail.com>
2022-10-30 00:05:39 +0530

    codes added

---
+++ dsa/random/jumpGame2.cpp
@@ -0,0 +1,13 @@
+int Solution::jump(vector<int> &nums) {
+    int n = nums.size();
+    vector <int> dp(n,INT_MAX);
+    dp[0] = 0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<=i+nums[i];j++){
+            if(j<n)
+                dp[j] = min(dp[j],1+dp[i]);
+        }
+    }
+    return dp[n-1];
+
+}
