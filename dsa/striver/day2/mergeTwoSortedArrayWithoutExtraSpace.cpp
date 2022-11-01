6f48d7503ee74d81591286f67d3c936e35e2263b
Shivang Mangal <shivangmangal@gmail.com>
2022-11-01 21:23:44 +0530

    dsa/striver/day2/mergeTwoSortedArrayWithoutExtraSpace.cpp MODIFIED

--- dsa/striver/day2/mergeTwoSortedArrayWithoutExtraSpace.cpp
+++ dsa/striver/day2/mergeTwoSortedArrayWithoutExtraSpace.cpp
@@ -1,58 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int merge(int arr[],int temp[],int left,int mid,int right)
-{
-    int inv_count=0;
-    int i = left;
-    int j = mid;
-    int k = left;
-    while((i <= mid-1) && (j <= right)){
-        if(arr[i] <= arr[j]){
-            temp[k++] = arr[i++];
-        }
-        else
-        {
-            temp[k++] = arr[j++];
-            inv_count = inv_count + (mid - i);
-        }
+void merge(int ar1[], int ar2[], int n, int m) {
+  // code here 
+  int gap = ceil((float)(n + m) / 2);
+  while (gap > 0) {
+    int i = 0;
+    int j = gap;
+    while (j < (n + m)) {
+      if (j < n && ar1[i] > ar1[j]) {
+        swap(ar1[i], ar1[j]);
+      } else if (j >= n && i < n && ar1[i] > ar2[j - n]) {
+        swap(ar1[i], ar2[j - n]);
+      } else if (j >= n && i >= n && ar2[i - n] > ar2[j - n]) {
+        swap(ar2[i - n], ar2[j - n]);
+      }
+      j++;
+      i++;
     }
-
-    while(i <= mid - 1)
-        temp[k++] = arr[i++];
-
-    while(j <= right)
-        temp[k++] = arr[j++];
-
-    for(i = left ; i <= right ; i++)
-        arr[i] = temp[i];
-    
-    return inv_count;
-}
-
-int merge_Sort(int arr[],int temp[],int left,int right)
-{
-    int mid,inv_count = 0;
-    if(right > left)
-    {
-        mid = (left + right)/2;
-
-        inv_count += merge_Sort(arr,temp,left,mid);
-        inv_count += merge_Sort(arr,temp,mid+1,right);
-
-        inv_count += merge(arr,temp,left,mid+1,right);
+    if (gap == 1) {
+      gap = 0;
+    } else {
+      gap = ceil((float) gap / 2);
     }
-    return inv_count;
+  }
 }
+int main() {
+  int arr1[] = {1,4,7,8,10};
+  int arr2[] = {2,3,9};
+  cout << "Before merge:" << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << arr1[i] << " ";
+  }
+  cout << endl;
+  for (int i = 0; i < 3; i++) {
+    cout << arr2[i] << " ";
+  }
+  cout << endl;
+  merge(arr1, arr2, 5, 3);
+  cout << "After merge:" << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << arr1[i] << " ";
+  }
+  cout << endl;
+  for (int i = 0; i < 3; i++) {
+    cout << arr2[i] << " ";
+  }
 
-int main()
-{
-    int arr[]={5,3,2,1,4};
-    int n=5;
-    int temp[n];
-    int ans = merge_Sort(arr,temp,0,n-1);
-    cout<<"The total inversions are "<<ans<<endl; 
-
-
-    return 0;
 }
