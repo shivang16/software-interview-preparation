6d0ba7ea2e614715df267eb6cbd364a0ac90765a
Shivang Mangal <shivangmangal@gmail.com>
2023-08-02 17:22:29 +0200

    dsa/trees/checkValidBST.cpp ADDED

---
+++ dsa/trees/checkValidBST.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void getInorder(TreeNode* root,vector <int> &v){
+        if(!root) return;
+        getInorder(root->left,v);
+        v.push_back(root->val);
+        getInorder(root->right,v);
+    }
+    bool isValidBST(TreeNode* root) {
+        vector <int> v;
+        getInorder(root,v);
+        for(int i=0;i<v.size()-1;i++){
+            if(v[i]>=v[i+1])
+                return false;
+        }
+        return true;
+    }
+};
