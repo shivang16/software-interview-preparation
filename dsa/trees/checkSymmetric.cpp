33c27a00c7afd2b9aa2ed3debc0385903b77e203
Shivang Mangal <shivangmangal@gmail.com>
2023-08-02 17:22:28 +0200

    dsa/trees/checkSymmetric.cpp ADDED

---
+++ dsa/trees/checkSymmetric.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkSymmetric(TreeNode* l,TreeNode* r){
+        if(!l && !r) return true;
+        if(!l || !r) return false;
+        if(l->val!=r->val) return false;
+        return checkSymmetric(l->left,r->right) && checkSymmetric(l->right,r->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return checkSymmetric(root->left,root->right);
+    }
+};
