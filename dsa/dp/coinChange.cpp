21f61c1e223d67b11027c8f85abc2cd944b4b06b
Shivang Mangal <shivangmangal@gmail.com>
2023-08-03 21:50:22 +0200

    dsa/dp/coinChange.cpp ADDED

---
+++ dsa/dp/coinChange.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int coinChange(vector<int>& c, int am) {
+        int n = c.size();
+        if(n==0)
+        {
+            if(am==0)
+                return 0;
+            return -1;
+        }
+        sort(c.begin(),c.end());
+        int dp[am+1];
+        for(int i=0;i<=am;i++)
+            dp[i] = INT_MAX;
+        //memset(dp,-1,sizeof(dp));
+        dp[0] = 0;
+        for(int i=c[0];i<=am;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i>=c[j] && dp[i-c[j]]!=INT_MAX)
+                    dp[i] = min(dp[i],1+dp[i-c[j]]);
+            }
+        }
+        return dp[am]==INT_MAX?-1:dp[am];
+        
+        
+        
+    }
+};
