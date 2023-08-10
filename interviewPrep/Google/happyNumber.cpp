4b20656925c93d95039aaa6ae973eedd853ccd8b
Shivang <shivangmangal@gmail.com>
2023-08-10 15:36:02 +0200

    interviewPrep/Google/happyNumber.cpp ADDED

---
+++ interviewPrep/Google/happyNumber.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_set <int> S;
+    bool getAns(int n){
+        if(n==1)
+            return true;
+        if(n==2||n==3 || S.find(n)!=S.end())
+            return false;
+        S.insert(n);
+        int val = 0;
+        while(n!=0){
+            int rem = n%10;
+            val += pow(rem,2);
+            n/=10;
+        }
+        return getAns(val);
+    }
+    bool isHappy(int n) {
+        S.clear();
+        return getAns(n);
+        
+    }
+};
