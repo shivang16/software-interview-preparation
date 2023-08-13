1e4db0fb3c5fca4f26d1ea854efad703d3a597ad
Shivang <shivangmangal@gmail.com>
2023-08-13 11:38:27 +0200

    interviewPrep/Google/battleShipOnTheBoard.cpp ADDED

---
+++ interviewPrep/Google/battleShipOnTheBoard.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/battleships-in-a-board/submissions/
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        int ans = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='X' && (i==0 || board[i-1][j]=='.') && (j==0 || board[i][j-1]=='.'))
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
