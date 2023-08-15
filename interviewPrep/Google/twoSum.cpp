772d9619a2501d6c6821add7743f1923f77f00b7
Shivang <shivangmangal@gmail.com>
2023-08-15 10:33:10 +0200

    interviewPrep/Google/twoSum.cpp ADDED

---
+++ interviewPrep/Google/twoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> ans;
+        unordered_map <int,int> M;
+        for(int i=0;i<nums.size();i++){
+            if(M.find(target-nums[i])!=M.end()){
+                ans.push_back(i);
+                ans.push_back(M[target-nums[i]]);
+            }
+            M[nums[i]] = i;
+        }
+        return ans;
+    }
+};
