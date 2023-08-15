8684764c9e1e106d0c0d2c51e1e9acae8a1c8eed
Shivang <shivangmangal@gmail.com>
2023-08-15 10:33:07 +0200

    interviewPrep/Google/rearrangeSpacesBetweenWords.cpp ADDED

---
+++ interviewPrep/Google/rearrangeSpacesBetweenWords.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/rearrange-spaces-between-words/description/
+class Solution {
+public:
+    vector <string> split(string text,string del){
+        vector <string> ans;
+        char* token = strtok(&text[0],&del[0]);
+        while(token){
+            ans.push_back(token);
+            token = strtok(NULL,&del[0]);
+        }
+        return ans;
+    }
+    string reorderSpaces(string text) {
+        vector <string> words = split(text," ");
+        int sz = 0;
+        for(auto i:words){
+            sz+=i.size();
+        }
+        int totalSpaces = text.size()-sz;
+        if(totalSpaces==0)
+            return text;
+        int totalWords = words.size();
+        int spaces;
+        if(totalWords>1)
+            spaces = totalSpaces/(totalWords-1);
+        else
+            spaces = totalSpaces;
+        // cout << totalSpaces << " " << totalWords << " " << spaces << endl;
+        string ans = "";
+        for(auto i:words){
+            ans+=i;
+            int j = spaces;
+            while(j-->0 && totalSpaces-->0){
+                ans+=' ';
+            }
+        }
+        while(totalSpaces-->0)
+            ans+=' ';
+        return ans;
+        
+    }
+};
