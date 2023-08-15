69207625906b9b876e0b7481d2232f387b91d435
Shivang <shivangmangal@gmail.com>
2023-08-15 10:33:09 +0200

    interviewPrep/Google/studentAttendanceRecord1.cpp ADDED

---
+++ interviewPrep/Google/studentAttendanceRecord1.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/student-attendance-record-i/submissions/
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int abTot = 0;
+        int curLate = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='L'){
+                curLate++;
+            }else{
+                if(s[i]=='A')
+                    abTot++;
+                curLate = 0;
+            }
+            if(curLate>=3)
+                return false;
+            if(abTot>=2)
+                return false;
+            
+        }
+        return true;
+    }
+};
