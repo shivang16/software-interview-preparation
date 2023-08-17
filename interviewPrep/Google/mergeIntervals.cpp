c440b0cdbd57d949494e0d14416dbe8a27731d71
Shivang <shivangmangal@gmail.com>
2023-08-17 21:11:01 +0200

    interviewPrep/Google/mergeIntervals.cpp ADDED

---
+++ interviewPrep/Google/mergeIntervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector <vector <int>> ans;
+        int l = intervals[0][0];
+        int r = intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(r>=intervals[i][0]){
+                r = max(r,intervals[i][1]);
+            }else{
+                ans.push_back({l,r});
+                l = intervals[i][0];
+                r = intervals[i][1];
+            }
+        }
+        ans.push_back({l,r});
+        return ans;
+    }
+};
