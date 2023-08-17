313622bfdbc38865c37cbb4cea0f09baaf71e5c3
Shivang <shivangmangal@gmail.com>
2023-08-17 21:11:19 +0200

    interviewPrep/Google/removeInterval.cpp ADDED

---
+++ interviewPrep/Google/removeInterval.cpp
@@ -0,0 +1,22 @@
+vector<vector<int>> ans;
+	
+	for(auto &i :  intervals)
+	{
+		int left = i[0];
+		int right = i[1];
+		if( (toBeRemoved[0] > right ) or (toBeRemoved[1] < left ))
+		{
+			ans.push_back({left, right});
+		}
+		else
+		{
+			if(left < toBeRemoved[0])
+			{
+				ans.push_back({left, toBeRemoved[0]});
+			}
+			if (right > toBeRemoved[1])
+			{
+				ans.push_back({toBeRemoved[1], right});
+			}
+		}
+	}
