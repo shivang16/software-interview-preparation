fd95a415555eda5b2ef49ea9a045b790f7962c02
Shivang <shivangmangal@gmail.com>
2023-08-17 21:10:47 +0200

    interviewPrep/Google/longestWordInDictThroughDeleting.cpp ADDED

---
+++ interviewPrep/Google/longestWordInDictThroughDeleting.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isSubSeq(string s,string t){
+        int i=0,j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return j==t.size();
+    }
+    string findLongestWord(string s, vector<string>& dictionary) {
+        string ans = "";
+        for(auto i:dictionary){
+            if(isSubSeq(s,i)){
+                if(ans.size()<i.size()){
+                    ans = i;
+                }else if(ans.size()==i.size())
+                    ans = min(ans,i);
+            }
+        }
+        return ans;
+    }
+};
