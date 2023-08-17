a4267f721ca2801a9892f2715b3a7175ea5ca2f8
Shivang <shivangmangal@gmail.com>
2023-08-17 21:11:10 +0200

    interviewPrep/Google/myCalender3.cpp ADDED

---
+++ interviewPrep/Google/myCalender3.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/my-calendar-iii/description/
+class MyCalendarThree {
+public:
+    map <int,int> M;
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int start, int end) {
+        M[start]++;
+        M[end]--;
+        int count = 0;
+        int mx = 0;
+        for(auto i:M){
+            count+=i.second;
+            mx = max(mx,count);
+        }
+        return mx;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(startTime,endTime);
+ */
