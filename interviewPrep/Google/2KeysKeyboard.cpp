cbbb1d57b6456d5355c8e5f6ad306dd03864b493
Shivang <shivangmangal@gmail.com>
2023-08-19 21:13:42 +0200

    interviewPrep/Google/2KeysKeyboard.cpp ADDED

---
+++ interviewPrep/Google/2KeysKeyboard.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/2-keys-keyboard/description/
+class Solution {
+public:
+    int minSteps(int n) {
+        int dp[n+1];
+        for(int i=0;i<=n;i++)
+            dp[i] = INT_MAX;
+        dp[0] = 0;
+        dp[1] = 0;
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if((i-j)%j==0){
+                    dp[i] = min(dp[i],dp[j]+(i-j)/j+1);
+                }
+            }
+        }
+        return dp[n];
+    }
+};
