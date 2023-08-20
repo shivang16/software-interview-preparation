af302e0ab01b60be60f5f460db312f788b6820ff
Shivang <shivangmangal@gmail.com>
2023-08-20 18:00:09 +0200

    interviewPrep/Google/containerWithMostWater.cpp ADDED

---
+++ interviewPrep/Google/containerWithMostWater.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size()-1;
+        int i=0, j=n;
+        int ans=0;
+        while(i <= j){
+            int area=0;
+            int ht = min(height[i], height[j]);
+            area = (j-i)*ht;
+            ans = max(ans, area);
+            if(height[i] > height[j]) j--;
+            else i++;
+        }
+
+        return ans;
+    }
+};
