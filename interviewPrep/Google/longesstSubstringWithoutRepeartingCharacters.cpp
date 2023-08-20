916c0fc3852ffae5fd94733a789d41c6c1cb1caa
Shivang <shivangmangal@gmail.com>
2023-08-20 18:00:16 +0200

    interviewPrep/Google/longesstSubstringWithoutRepeartingCharacters.cpp ADDED

---
+++ interviewPrep/Google/longesstSubstringWithoutRepeartingCharacters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.length();
+        int l=0,r=0;
+        int cnt=0;
+        int a[256]={0};
+        while(r<n){
+            if(a[s[r]]==0){
+                a[s[r]]++;
+                cnt=max(cnt,r-l+1);
+                r++;
+            }
+            else{
+                a[s[l]]--;
+                l++;
+                    
+            }
+        }
+        return cnt;
+    }
+};
