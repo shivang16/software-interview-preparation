ff82bf18426522d7740301e8d85e7c803f11d624
Shivang <shivangmangal@gmail.com>
2023-08-20 18:00:15 +0200

    interviewPrep/Google/kRadiusSubarrayAverages.cpp ADDED

---
+++ interviewPrep/Google/kRadiusSubarrayAverages.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int n = nums.size();
+        int low = 0,high = 0;
+        vector <int> ans;
+        if(k==0) return nums;
+        if(k>=n-1){
+            for(int i=0;i<n;i++)
+                ans.push_back(-1);
+            return ans;
+        }
+        for(int i=0;i<k;i++)
+            ans.push_back(-1);
+        long long sum = 0;
+        int newK = 2*k+1;
+        while(high<n){
+            sum+=nums[high];
+            if(high-low+1==newK){
+                ans.push_back(sum/newK);
+                sum-=nums[low];
+                low++;
+            }
+            high++;
+        }
+        for(int i=0;i<k;i++)
+            ans.push_back(-1);
+        return ans;
+    }
+};
