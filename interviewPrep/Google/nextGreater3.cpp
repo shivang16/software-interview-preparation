87d3ccfda895df1aa5eb2d691713c0deedfb9944
Shivang <shivangmangal@gmail.com>
2023-08-20 18:00:31 +0200

    interviewPrep/Google/nextGreater3.cpp ADDED

---
+++ interviewPrep/Google/nextGreater3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        if(n<10)
+            return -1;
+        string s = to_string(n);
+        bool val = next_permutation(s.begin(),s.end());
+        if(!val)
+            return -1;
+        long long  m = 0;
+        for(int i=0;i<s.length();i++)
+           m = m*10+(s[i]-'0');
+        if(m>INT_MAX)
+            return -1;
+        
+        return (int)(m);
+        // return 0;
+    }
+};
