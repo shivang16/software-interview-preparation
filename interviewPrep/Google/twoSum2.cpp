e4f4afa11d6288abbd09c33863d948a995876f53
Shivang <shivangmangal@gmail.com>
2023-08-20 18:00:42 +0200

    interviewPrep/Google/twoSum2.cpp ADDED

---
+++ interviewPrep/Google/twoSum2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> ans;
+        int l=0,r=nums.size();
+        while(r>l){
+            if(nums[l]+nums[r]==target){
+                ans.push_back(l+1);
+                ans.push_back(r+1);
+                
+                return ans;
+            }else if(nums[l]+nums[r]>target)
+                r--;
+            else
+                l++;
+        }
+        return ans;
+    }
+};
