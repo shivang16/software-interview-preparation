5fd70d77e98c69c71583b79ce352a4cdfcd5d0e5
Shivang <shivangmangal@gmail.com>
2023-08-20 18:00:19 +0200

    interviewPrep/Google/maximumAverageSubarray1.cpp ADDED

---
+++ interviewPrep/Google/maximumAverageSubarray1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n  = nums.size();
+        double ans = INT_MIN;
+        int sum = 0;
+        int low = 0, high = 0;
+        while(high<n){
+            sum+=nums[high];
+            if(high-low+1==k){
+                double avg = (double)sum/k;
+                ans = max(ans,avg);
+                sum-=nums[low];
+                low++;
+            }
+            high++;
+        }
+        return ans;
+    }
+};
