6ec3dadd35653f65e4311ed845f8f747ae093a10
Shivang <shivangmangal@gmail.com>
2023-08-22 20:27:33 +0200

    interviewPrep/Google/bestTimeToBuyAndSellStock3.cpp ADDED

---
+++ interviewPrep/Google/bestTimeToBuyAndSellStock3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getAns(vector<int>& prices,vector <vector <int>> &dp,int ind,int k){
+        if(ind>=prices.size() || k<=0) return 0;
+        if(dp[ind][k]!=-1) return dp[ind][k];
+        int notIncluded = getAns(prices,dp,ind+1,k); // not considering this element 
+        int included = 0;
+        if(k%2!=0){
+            included = prices[ind]+getAns(prices,dp,ind+1,k-1);
+        }else{
+            included = -prices[ind]+getAns(prices,dp,ind+1,k-1);
+        }
+        return dp[ind][k] = max(notIncluded,included);
+    }
+    int maxProfit(vector<int>& prices) {
+        int k = 2*2; // for buying and selling
+        int n = prices.size();
+        vector <vector <int>> dp(n,vector <int> (k+1,-1));
+        return getAns(prices,dp,0,k);
+    }
+};
