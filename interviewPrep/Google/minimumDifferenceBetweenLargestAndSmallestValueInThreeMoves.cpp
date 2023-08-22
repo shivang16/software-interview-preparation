44846e963498b752631b899f9b8dc816b1e88d28
Shivang <shivangmangal@gmail.com>
2023-08-22 07:47:17 +0200

    interviewPrep/Google/minimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.cpp ADDED

---
+++ interviewPrep/Google/minimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.cpp
@@ -0,0 +1,18 @@
+#define ll long long
+class Solution {
+public:
+    int minDifference(vector<int>& A) {
+        int ans = INT_MAX;
+        int l = A.size();
+        if(l<=3)
+            return 0;
+        sort(A.begin(),A.end());
+        ans = min(ans,A[l-4]-A[0]);
+        ans = min(ans,A[l-1]-A[3]);
+        ans = min(ans,A[l-3]-A[1]);
+        ans = min(ans,A[l-2]-A[2]);
+        return ans;
+        
+        
+    }
+};
