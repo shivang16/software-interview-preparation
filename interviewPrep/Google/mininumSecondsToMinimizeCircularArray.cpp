44c8bce2cc7ef43ec603d3df04517244a3608377
Shivang <shivangmangal@gmail.com>
2023-08-22 07:47:18 +0200

    interviewPrep/Google/mininumSecondsToMinimizeCircularArray.cpp ADDED

---
+++ interviewPrep/Google/mininumSecondsToMinimizeCircularArray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumSeconds(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map <int,vector <int>> Mp;
+        for(int i=0;i<n;i++){
+            Mp[nums[i]].push_back(i);
+        }
+        int ans = INT_MAX;
+        for(auto [val,pos]: Mp){
+            pos.push_back(pos[0]+n);
+            int time = INT_MIN;
+            for(int i=0;i<pos.size()-1;i++){
+                time = max(time,(pos[i+1]-pos[i])/2);
+            }
+            ans = min(ans,time);
+        }
+        return ans;
+    }
+};
