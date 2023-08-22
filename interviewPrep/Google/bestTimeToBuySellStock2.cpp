96f0812cb107b97b94e233183b177f19f150a21d
Shivang <shivangmangal@gmail.com>
2023-08-22 20:27:37 +0200

    interviewPrep/Google/bestTimeToBuySellStock2.cpp ADDED

---
+++ interviewPrep/Google/bestTimeToBuySellStock2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int min = prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>min){
+                ans+= (prices[i]-min);
+            }
+            min = prices[i];
+        }
+        return ans;
+    }
+};
