c6bc49c2d452fb32a160afac571459ded7cb9e13
Shivang <shivangmangal@gmail.com>
2023-08-22 20:28:09 +0200

    interviewPrep/Google/validParanthesis.cpp ADDED

---
+++ interviewPrep/Google/validParanthesis.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> S;
+        for(auto i:s){
+            if(i=='(' || i=='{' || i=='[')
+                S.push(i);
+            else{
+                if(S.empty()) return false;
+                char c = S.top();
+                if(i==')' && c!='(') return false;
+
+                if(i=='}' && c!='{') return false;
+
+                if(i==']' && c!='[') return false;
+                S.pop();
+            }
+        }
+        return S.empty();
+    }
+};
