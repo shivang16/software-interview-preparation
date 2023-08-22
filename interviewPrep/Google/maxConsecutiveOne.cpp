894e9f7aaf69088ab0c9236e42cc1dfd40536cd4
Shivang <shivangmangal@gmail.com>
2023-08-22 20:27:51 +0200

    interviewPrep/Google/maxConsecutiveOne.cpp ADDED

---
+++ interviewPrep/Google/maxConsecutiveOne.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans = 0;
+        int count = 0;
+        int l = 0,n = nums.size();
+        while(l<n){
+            if(nums[l]==1){
+                int r = l+1;
+                while(r<n && nums[r]==1)
+                    r++;
+                ans = max(ans,r-l);
+                l=r;
+            }else
+                l++;
+        }
+        return ans;
+    }
+};
