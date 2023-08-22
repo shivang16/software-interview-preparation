d639601c65b1c9816e030ad39d7ec8a0781dfcda
Shivang <shivangmangal@gmail.com>
2023-08-22 20:27:52 +0200

    interviewPrep/Google/maxConsecutiveOne3.cpp ADDED

---
+++ interviewPrep/Google/maxConsecutiveOne3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int zs = 0;
+        int n = nums.size(), l=0,r=0,ans=0;
+        while(r<n){
+            if(nums[r]==0)
+                zs++;
+            while(zs>k){
+                if(nums[l]==0)
+                    zs--;
+                l++;
+            }
+            ans = max(ans,r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
