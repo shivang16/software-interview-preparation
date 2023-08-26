30b58289f3bda8bd35de6a3d8186a4c463f38b26
Shivang <shivangmangal@gmail.com>
2023-08-26 20:23:37 +0200

    interviewPrep/Google/findTwoNonOverlappingSubarrayEachWithSumTarget.cpp ADDED

---
+++ interviewPrep/Google/findTwoNonOverlappingSubarrayEachWithSumTarget.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSumOfLengths(vector<int>& arr, int target) {
+        unordered_map <long long,int>M;
+        long long sum = 0;
+        M[0] = -1;
+        for(int i=0;i<arr.size();i++){
+            sum=(long long)(sum+arr[i]);
+            M[sum] = i;
+        }
+        sum = 0;
+        int leftSize = INT_MAX, ans=INT_MAX;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+            if(M.find(sum-target)!=M.end()){
+                leftSize = min(leftSize,i-M[sum-target]+1);
+            }
+
+            if(M.find(sum+target)!=M.end() && leftSize!=INT_MAX){
+                ans = min(ans,(M[sum+target]-i-1)+leftSize);
+            }
+        }
+        return ans==INT_MAX?-1:ans;
+
+    }
+};
