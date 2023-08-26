5c38270f5386a23a5f68e3ec2230da3b5e67ea49
Shivang <shivangmangal@gmail.com>
2023-08-26 20:24:00 +0200

    interviewPrep/Google/productOfLastKNumbers.cpp ADDED

---
+++ interviewPrep/Google/productOfLastKNumbers.cpp
@@ -0,0 +1,30 @@
+class ProductOfNumbers {
+public:
+    vector <int> products; 
+    ProductOfNumbers() {
+        products.push_back(1);
+    }
+    
+    void add(int num) {
+        if(num==0){
+            products.clear();
+            products.push_back(1);
+        }
+        else
+            products.push_back(products.back()*num);
+    }
+    
+    int getProduct(int k) {
+        int crntInd = products.size()-1;
+        int reqInd = crntInd-k;
+        if(reqInd<0) return 0;
+        return products[crntInd]/products[reqInd];
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
