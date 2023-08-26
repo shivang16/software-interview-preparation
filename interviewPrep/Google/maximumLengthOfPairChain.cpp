100ac7de16b0d29d6d9122dec48e59580bf857ef
Shivang <shivangmangal@gmail.com>
2023-08-26 20:23:48 +0200

    interviewPrep/Google/maximumLengthOfPairChain.cpp ADDED

---
+++ interviewPrep/Google/maximumLengthOfPairChain.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end());
+        int n = pairs.size();
+        vector <int> dp(n,1);
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(pairs[i][0] > pairs[j][1])
+                    dp[i] = max(dp[i],1+dp[j]);
+            }
+        }
+        return dp[n-1];
+    }
+};
