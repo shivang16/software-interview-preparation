4db34a5c3b5eb63af15a9d4750fbbde1e3c8bfd6
Shivang <shivangmangal@gmail.com>
2023-08-28 14:53:44 +0200

    interviewPrep/Google/jumpGame.cpp ADDED

---
+++ interviewPrep/Google/jumpGame.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int mx = 0;
+        for(int i=0;i<n;i++){
+            if(i>mx){
+                return false;
+            }
+           mx = max(mx,i+nums[i]);
+        }
+        return mx>=n-1;
+    }
+};
+
