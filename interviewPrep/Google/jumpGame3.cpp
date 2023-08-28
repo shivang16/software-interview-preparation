da59942d25c1d955330a12fb7252a10b1995d1c1
Shivang <shivangmangal@gmail.com>
2023-08-28 14:53:50 +0200

    interviewPrep/Google/jumpGame3.cpp ADDED

---
+++ interviewPrep/Google/jumpGame3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool dfs(vector <int>& arr,int start,vector <bool> &vis){
+        if(start<0 || start>=arr.size()) return false;
+        if(arr[start]==0) return true;
+        if(vis[start]) return false;
+        vis[start] = true;
+        return dfs(arr,start-arr[start],vis) || dfs(arr,start+arr[start],vis);
+    }
+    bool canReach(vector<int>& arr, int start) {
+        vector <bool> vis(arr.size(),false);
+        return dfs(arr,start,vis);        
+    }
+};
