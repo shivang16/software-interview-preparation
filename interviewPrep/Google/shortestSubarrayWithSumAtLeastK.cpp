67d7410c2ec266c2889b5c528384101475778089
Shivang <shivangmangal@gmail.com>
2023-08-29 18:25:33 +0200

    interviewPrep/Google/shortestSubarrayWithSumAtLeastK.cpp ADDED

---
+++ interviewPrep/Google/shortestSubarrayWithSumAtLeastK.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    typedef long long ll;
+    int shortestSubarray(vector<int>& A, int K) {
+        int n = A.size();
+        priority_queue <pair <ll,int>,vector <pair<ll,int>>,greater <pair <ll,int>>> Q;
+        int ans = INT_MAX;
+        ll sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=A[i];
+            if(sum>=K)
+                ans = min(ans,i+1);
+            while(!Q.empty() && sum - Q.top().first >=K)
+            {
+                auto x = Q.top();
+                ans = min(ans,i-x.second);
+                Q.pop();
+            }
+            Q.push({sum,i});
+        }
+        return ans==INT_MAX?-1:ans;
+    }
+    };
