3ee95b19f7229be17950241360c11baea4aca683
Shivang <shivangmangal@gmail.com>
2023-08-31 08:02:52 +0200

    interviewPrep/Google/rangeSumQuery2DImMutable.cpp ADDED

---
+++ interviewPrep/Google/rangeSumQuery2DImMutable.cpp
@@ -0,0 +1,30 @@
+class NumMatrix {
+    private: 
+        vector <vector <int>> prefix;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(),n=matrix[0].size();
+        prefix.resize(m+1,vector <int> (n+1,0));
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                prefix[i][j] = prefix[i-1][j]+prefix[i][j-1]+matrix[i-1][j-1]-prefix[i-1][j-1];
+            }
+        }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++)
+        //         cout << prefix[i][j] << " ";
+        //     cout << endl;
+        // }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2+1][col2+1]-prefix[row1][col2+1]-prefix[row2+1][col1]+prefix[row1][col1];
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
