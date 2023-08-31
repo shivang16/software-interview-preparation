7dd0d92df816a4dac0aa362686b11b0831787f07
Shivang <shivangmangal@gmail.com>
2023-08-31 19:15:13 +0200

    interviewPrep/Google/longestSubstringWithAtMostKDistinctCharacters.cpp ADDED

---
+++ interviewPrep/Google/longestSubstringWithAtMostKDistinctCharacters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /**
+     * @param s: A string
+     * @param k: An integer
+     * @return: An integer
+     */
+    int lengthOfLongestSubstringKDistinct(string &s, int k) {
+        // write your code here
+        if(k==0) return 0;
+        unordered_map <char,int> Mp;
+        int n = s.size();
+        int l = 0, r = 0;
+        int ans = 0;
+        while(r<n){
+            Mp[s[r]]++;
+            while(l<r && Mp.size()>k){
+                Mp[s[l]]--;
+                if(Mp[s[l]]==0)
+                    Mp.erase(s[l]);
+                l++;
+            }
+            ans = max(ans,r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
