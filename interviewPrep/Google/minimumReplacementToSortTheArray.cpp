a38769a16d80828bda2e19c92244f6c1c8af117e
Shivang <shivangmangal@gmail.com>
2023-08-31 08:02:46 +0200

    interviewPrep/Google/minimumReplacementToSortTheArray.cpp ADDED

---
+++ interviewPrep/Google/minimumReplacementToSortTheArray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+        long long ans = 0;
+        int n = nums.size();
+        int prev = INT_MAX;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>prev){
+                int x = nums[i]%prev;
+                long long y = nums[i]/prev;
+                ans+=(y);
+                if(x==0){
+                    ans--;
+                }else{
+                    prev = (nums[i])/(y+1);
+                }
+                // prev = x==0?prev:x;
+            }else{
+                prev = nums[i];
+            }
+        }
+        return ans;
+    }
+};
