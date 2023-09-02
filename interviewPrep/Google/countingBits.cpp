6ce0f40b2e8c2a4043e1fb1d88cee270c93257ef
Shivang <shivangmangal@gmail.com>
2023-09-02 08:18:14 +0200

    interviewPrep/Google/countingBits.cpp ADDED

---
+++ interviewPrep/Google/countingBits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector <int> dp(n+1,0);
+        dp[0] = 0;
+        if(n==0) return dp;
+        dp[1] = 1;
+        if(n==1) return dp;
+        dp[2] = 1;
+        if(n==2) return dp;
+        int p = 1;
+        for(int i=3;i<=n;i++){
+            int val = pow(2,p);
+            int val2 = pow(2,p+1);
+            if(val2==i){
+                dp[i] = 1;
+                p++;
+            }else{
+                dp[i] = 1+dp[i-val];
+            }
+        }
+        return dp;
+    }
+};
