05cee2661697cbebeba8baa1e68531db0163b5df
Shivang <shivangmangal@gmail.com>
2023-09-02 19:16:10 +0200

    interviewPrep/Google/minimumArea2.cpp ADDED

---
+++ interviewPrep/Google/minimumArea2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    long long getDist(vector <int> &p1,vector <int> &p2){
+        return (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]);
+    }
+
+    double minAreaFreeRect(vector<vector<int>>& points) {
+        double ans = DBL_MAX;
+        int n = points.size();
+        vector <vector <long long>> dis(n,vector <long long> (n,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dis[i][j] = getDist(points[i],points[j]);
+            }
+        }
+        for(long i=0;i<n-3;i++){
+            for(long j=i+1;j<n-2;j++){
+                for(long k=j+1;k<n-1;k++){
+                    for(long l=k+1;l<n;l++){
+                        long long ij = dis[i][j];
+                        long long ik = dis[i][k];
+                        long long il = dis[i][l];
+                        long long jk = dis[j][k];
+                        long long jl = dis[j][l];
+                        long long kl = dis[k][l];
+                        if(ij && ik && il && jk && jl && kl){
+                            if(ij+ik == il && jl==ik && kl==ij && kl+jl==jk){
+                                double area = (double)sqrt(ij)*(double)sqrt(ik);
+                                ans = min(ans,area);
+                            }else if(ik+il == ij && il==jk && ik==jl && jl+jk==kl){
+                                double area = (double)sqrt(ik)*(double)sqrt(il);
+                                ans = min(ans,area);
+                            }else if(ij+il == ik && il==jk && kl==ij && jk+kl==jl){
+                                double area = (double)sqrt(ij)*(double)sqrt(il);
+                                ans = min(ans,area);
+                            }
+                        }                  
+                    }
+                }
+            }
+        }
+        return ans==DBL_MAX?0:ans;    
+    }
+};
