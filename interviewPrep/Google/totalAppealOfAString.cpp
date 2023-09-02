4882dc3b2c0d66c90316c542808d65db914cd9fc
Shivang <shivangmangal@gmail.com>
2023-09-02 19:16:16 +0200

    interviewPrep/Google/totalAppealOfAString.cpp ADDED

---
+++ interviewPrep/Google/totalAppealOfAString.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long appealSum(string s) {
+        unordered_map <char,int> mp;
+        long long ans = 0;
+        int n = s.size();
+        vector <long long> dp(n+1,0);
+        for(int i=1;i<=n;i++){
+            dp[i] = dp[i-1];
+            if(mp.count(s[i-1])==0){
+                dp[i]+=i;
+            }else{
+                dp[i]+=(i-mp[s[i-1]]);
+            }
+            mp[s[i-1]] = i;
+            ans+=dp[i];
+        }
+        return ans;
+        
+    }
+};
