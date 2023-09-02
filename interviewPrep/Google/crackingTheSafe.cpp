e8c23c6a0deb1e4aac59b94abf954126c8f6ed5d
Shivang <shivangmangal@gmail.com>
2023-09-02 08:18:16 +0200

    interviewPrep/Google/crackingTheSafe.cpp ADDED

---
+++ interviewPrep/Google/crackingTheSafe.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    void dfs(int k,int n,unordered_set <string> &vis,string &ans,string temp){
+        for(int i=0;i<k;i++){
+            string s = temp+to_string(i);
+            if(!vis.count(s)){
+                vis.insert(s);
+                dfs(k,n,vis,ans,s.substr(1));
+                ans+=to_string(i);
+            }
+        }
+    }
+    string crackSafe(int n, int k) {
+        string ans = "",temp = "";
+        unordered_set <string> vis;
+        for(int i=0;i<n-1;i++){
+            temp+="0";
+        }
+        dfs(k,n,vis,ans,temp);
+        return ans+temp;
+    }
+
+};
