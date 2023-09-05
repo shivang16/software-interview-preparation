7242005702967be9aa5a5b140cd94254b3b1770a
Shivang <shivangmangal@gmail.com>
2023-09-05 18:36:54 +0200

    interviewPrep/Google/nextGreaterElement.cpp ADDED

---
+++ interviewPrep/Google/nextGreaterElement.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/next-greater-element-i/
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n2 = nums2.size();
+        vector <int> ng(n2,-1);
+        stack <int> S;
+        unordered_map <int,int> M;
+        for(int i=0;i<n2;i++){
+            M[nums2[i]] = i;
+            while(!S.empty() && nums2[S.top()]<nums2[i]){
+                ng[S.top()] = nums2[i];
+                S.pop();
+            }
+            S.push(i);
+        }
+        vector <int> ans;
+        for(auto i:nums1){
+            ans.push_back(ng[M[i]]);
+        }
+        return ans;
+        
+    }
+};
