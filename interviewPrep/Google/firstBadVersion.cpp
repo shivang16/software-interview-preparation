35e7fc03b8dff69c83d9de598ee33151d93af36d
Shivang <shivangmangal@gmail.com>
2023-09-05 18:06:05 +0200

    interviewPrep/Google/firstBadVersion.cpp MODIFIED

--- interviewPrep/Google/firstBadVersion.cpp
+++ interviewPrep/Google/firstBadVersion.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/first-bad-version/
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 // https://leetcode.com/problems/first-bad-version/description/
