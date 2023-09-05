b0c0f4ac0abb9f553a8cdc3c22805b7a3e9e6c16
Shivang <shivangmangal@gmail.com>
2023-09-05 18:31:41 +0200

    interviewPrep/Google/numberOfProvinces.cpp MODIFIED

--- interviewPrep/Google/numberOfProvinces.cpp
+++ interviewPrep/Google/numberOfProvinces.cpp
@@ -1,55 +1,53 @@
-// https://leetcode.com/problems/number-of-provinces/description/
-
-
-class Solution {
-public:
+// https://leetcode.com/problems/number-of-provinces/submissions/
+class DisjointSet{
+private:
     vector <int> parent;
     vector <int> size;
-    int n;
-    int findParent(int p){
-        if(parent[p]==p)
-            return p;
-        return parent[p] = findParent(parent[p]);
-    }
-
-    void findUnionBySize(int u,int v){
-        int pu = findParent(u);
-        int pv = findParent(v);
-        // cout << u << " " << v << " " << pu << " " << pv << endl;
-        if(pu==pv) return;
-        int su = size[pu];
-        int sv = size[pv];
-        // cout << su << " " << sv << endl;
-        if(su<sv){
-            parent[pu] = pv;
-            size[pv]+=size[u]; 
-        }else{
-            parent[pv] = pu;
-            size[pu]+=size[v];
-        }
-    }
-    
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        n = isConnected.size();
+    int N;
+public:
+    DisjointSet(int n){
+        N = n;
         for(int i=0;i<n;i++){
             parent.push_back(i);
             size.push_back(1);
         }
+    }
+    int findParent(int u){
+        if(parent[u]==u) return u;
+        return parent[u] = findParent(parent[u]);
+    }
 
+    bool findUnion(int u,int v){
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if(pu==pv) return false;
+        int su = size[pu];
+        int sv = size[pv];
+        if(su>sv){
+            parent[pv] = pu;
+            size[su]+=sv;
+        }else{
+            parent[pu] = pv;
+            size[sv]+=su;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet ds(n);
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(isConnected[i][j])
-                    findUnionBySize(i,j);
+                    ds.findUnion(i,j);
             }
         }
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            if(findParent(i)==i){
-                ans++;
-                cout << i << endl;
-            }
-                
-        }
-        return ans;
+        unordered_set <int> st;
+        for(int i=0;i<n;i++)
+            st.insert(ds.findParent(i));
+        return st.size();
     }
 };
