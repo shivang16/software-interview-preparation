a691711a420582484781c61bc4b7122a9c55ea2f
Shivang <shivangmangal@gmail.com>
2023-09-05 18:02:32 +0200

    interviewPrep/Google/FindWinnerInTicTacToe.cpp MODIFIED

--- interviewPrep/Google/FindWinnerInTicTacToe.cpp
+++ interviewPrep/Google/FindWinnerInTicTacToe.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/submissions/
+// https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/submissions
 class Solution {
 public:
     string tictactoe(vector<vector<int>>& M) {
