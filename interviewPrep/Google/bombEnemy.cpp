61906d335603fed5b5f267ce27a426c291e7bb9d
Shivang <shivangmangal@gmail.com>
2023-09-05 18:03:10 +0200

    interviewPrep/Google/bombEnemy.cpp MODIFIED

--- interviewPrep/Google/bombEnemy.cpp
+++ interviewPrep/Google/bombEnemy.cpp
@@ -1,34 +1,43 @@
+// https://www.lintcode.com/problem/553/description
 class Solution {
-private:
-    int M, N;
-    int dirs[4][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
-    int count(vector<vector<char>>& grid, int x, int y, int dir[2]) {
-        int ans = 0;
-        while (true) {
-            x += dir[0];
-            y += dir[1];
-            if (x < 0 || x >= M || y < 0 || y >= N || grid[x][y] == 'W') break;
-            if (grid[x][y] == 'E') ++ans;
-        }
-        return ans;
-    }
-    int count(vector<vector<char>>& grid, int x, int y) {
-        int ans = 0;
-        for (auto &dir : dirs) ans += count(grid, x, y, dir);
-        return ans;
-    }
 public:
-    int maxKilledEnemies(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) return 0;
-        M = grid.size();
-        N = grid[0].size();
-        int ans = 0;
-        for (int i = 0; i < M; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (grid[i][j] != '0') continue;
-                ans = max(ans, count(grid, i, j));
-            }
+    /**
+     * @param grid: Given a 2D grid, each cell is either 'W', 'E' or '0'
+     * @return: an integer, the maximum enemies you can kill using one bomb
+     */
+    int m,n;
+    vector <vector <int>> dp;
+    int enemyCount = 0;
+    void update(vector <vector <char>>& grid,int x,int y){
+        if(grid[x][y]=='E'){
+            enemyCount++;
+        }else if(grid[x][y]=='0'){
+            dp[x][y]+= enemyCount;
+        }else{
+            enemyCount = 0;
         }
+    }
+    int maxKilledEnemies(vector<vector<char>> &grid) {
+        // write your code here
+        if(!grid.size() || !grid[0].size()) return 0;
+        int m = grid.size(), n = grid[0].size();
+        dp.resize(m,vector <int> (n,0));
+        for(int i=0;i<m;i++){
+            enemyCount = 0;
+            for(int j=0;j<n;j++) update(grid,i,j);
+            enemyCount = 0;
+            for(int j=n-1;j>=0;j--) update(grid,i,j);
+        }
+        for(int j=0;j<n;j++){
+            enemyCount = 0;
+            for(int i=0;i<m;i++) update(grid,i,j);
+            enemyCount = 0;
+            for(int i=m-1;i>=0;i--) update(grid,i,j);
+        }
+        int ans = 0;
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                ans = max(ans,dp[i][j]);
         return ans;
     }
 };
