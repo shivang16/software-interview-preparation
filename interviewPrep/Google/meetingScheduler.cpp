b110c22cc4cbad55be00a6c970c2a3cb01942950
Shivang <shivangmangal@gmail.com>
2023-09-05 18:24:34 +0200

    interviewPrep/Google/meetingScheduler.cpp MODIFIED

--- interviewPrep/Google/meetingScheduler.cpp
+++ interviewPrep/Google/meetingScheduler.cpp
@@ -1,44 +1,53 @@
-vector <int> meetingScheduler(vector <vector <int>> &slot1,vector <vector <int>> &slot2, int duration){
-    map <int,int> M;
-    for(auto i:slot1){
-        M[i[0]]++;
-        M[i[1]]--;
-    }
-    for(auto i:slot2){
-        M[i[0]]++;
-        M[i[1]]--;
-    }
-    vector <int> ans;
-    int count = 0,l=-1;
-    for(auto i:M){
-        count+=i.second;
-        if(l!=-1){
-            int diff = i.first-l;
-            if(diff>=duration){
-                ans.push_back(l);
-                ans.push_back(l+duration);
-                return ans;
-            }else{
-                l = -1;
+// https://www.lintcode.com/problem/3653/
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param slots1: The availability schedule of one.
+     * @param slots2: The availability schedule of one.
+     * @param duration: The expected duration of the meeting.
+     * @return: The earliest and appropriate meeting time in the interval for them.
+     */
+    Interval earliestAppropriateDuration(vector<Interval> &slots1, vector<Interval> &slots2, int duration) {
+        map <int,int> mp;
+        for(auto i:slots1){
+            mp[i.start]++;
+            mp[i.end]--;
+        }
+        for(auto i:slots2){
+            mp[i.start]++;
+            mp[i.end]--;
+        }
+        Interval ans(-1,-1);
+        
+        int count = 0,l=-1;
+        for(auto i:mp){
+            count+=i.second;
+            if(l!=-1){
+                int diff = i.first-l;
+                if(diff>=duration){
+                    return Interval(l,l+duration);
+                }else{
+                    l = -1;
+                }
+            }
+            if(count==2){
+                l = i.first;
             }
         }
-        if(count==2){
-            l = i.first;
-        }
+        return ans;
+        
+        // --- write your code here ---
     }
-    return ans;
-}
-void print(vector <int>& ans){
-    for(auto i:ans)
-        cout << i << " ";
-    cout << endl;
-}
-int main() {
-    vector <vector <int>> slots1 = {{10,50},{60,120},{140,210}}, slots2 = {{0,15},{60,70}}; int duration1 = 8;
-    vector <int> ans1 = meetingScheduler(slots1,slots2,duration1);
-    print(ans1);
-    vector <vector <int>> slots3 = {{10,50},{60,120},{140,210}}, slots4 = {{0,15},{60,70}}; int duration2 = 12;
-    vector <int> ans2 = meetingScheduler(slots3,slots4,duration2);
-    print(ans2);
-    
-}
+};
