c2e8bb34b3c575b68b1e1a9f152ed6b72e154066
Shivang <shivangmangal@gmail.com>
2023-09-05 18:31:27 +0200

    interviewPrep/Google/numberOfDiceRollWithGivenSum.cpp MODIFIED

--- interviewPrep/Google/numberOfDiceRollWithGivenSum.cpp
+++ interviewPrep/Google/numberOfDiceRollWithGivenSum.cpp
@@ -1,27 +1,22 @@
-// https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/description/
+// https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/submissions/
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
-        int mod = 1e9+7;
         int dp[n+1][target+1];
-
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=target;j++)
-                dp[i][j] = 0;
-        }
-        for(int i=1;i<=min(k,target);i++)
+        memset(dp,0,sizeof(dp));
+        int mod = 1e9+7;
+        for(int i=1;i<=min(target,k);i++)
             dp[1][i] = 1;
 
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=target;j++){
-                for(int m=1;m<=k;m++){
-                    if(j-m>=0){
-                        dp[i][j] = (dp[i][j]+dp[i-1][j-m])%mod; 
+        for(int diceCount=2;diceCount<=n;diceCount++){
+            for(int sum=1;sum<=target;sum++){
+                for(int faceValue=1;faceValue<=k;faceValue++){
+                    if(sum>=faceValue){
+                        dp[diceCount][sum] = (dp[diceCount][sum]+dp[diceCount-1][sum-faceValue])%mod;
                     }
                 }
             }
         }
         return dp[n][target];
-        
     }
 };
