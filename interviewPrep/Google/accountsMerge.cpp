5bc6b45edb7ef02abaabfd7c47161d64d13b2b6f
Shivang <shivangmangal@gmail.com>
2023-09-05 18:02:34 +0200

    interviewPrep/Google/accountsMerge.cpp MODIFIED

--- interviewPrep/Google/accountsMerge.cpp
+++ interviewPrep/Google/accountsMerge.cpp
@@ -1,68 +1,73 @@
+// https://leetcode.com/problems/accounts-merge/
+class DisjointSet{
+    private: 
+        int N;
+        vector <int> parent;
+        vector <int> size;
+    public: 
+        DisjointSet(int n){
+            N = n;
+            for(int i=0;i<n;i++){
+                parent.push_back(i);
+                size.push_back(1);
+            }
+        }
+
+        int getParent(int node){
+            if(parent[node]==node) return node;
+            return parent[node] = getParent(parent[node]);
+        }
+
+        bool findUnion(int u,int v){
+            int pu = getParent(u);
+            int pv = getParent(v);
+            if(pu==pv) return false;
+            int su = size[pu];
+            int sv = size[pv];
+            if(su>sv){
+                parent[pv] = pu;
+                size[pu]+=size[pv]; 
+            }else{
+                parent[pu] = pv;
+                size[pv]+=size[pu];
+            }
+            return true;
+        }
+};
+
 class Solution {
 public:
-    // union find in emails
-    int find(int i,vector<int>&parent,int &deep,vector<int>&rank){
-        if(i==parent[i]) return i;
-        parent[i]=find(parent[i],parent,++deep,rank);
-        rank[i]+=deep;
-        return parent[i];
-    }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-              //  eclass Solution {
-public:
-    vector <int> parent;
-    vector <int> size;
-    int findParent(int p){
-        if(parent[p]==p)
-            return p;
-        return parent[p] = findParent(parent[p]);
-    }
-
-…};mail name
-        vector<pair<string,string>>v;
-        for(auto &vec:accounts)
-          for(int i=1;i<vec.size();i++)
-		     v.push_back({vec[i],vec[0]});
-             
-        vector<int>parent(v.size());
-        vector<int>rank(v.size(),1);
-		
-        for(int i=0;i<parent.size();i++) parent[i]=i;
-        
-        unordered_map<string,int>mp;
-        vector<bool>vis(parent.size());
-        
-        int cnt=0,tp;
-         // cnt  uniquely differentiate each email (for ex= first email key is 1 ,second has 2 and so on)
-        for(auto &vec:accounts){
-             int p;
-             if(mp.count(v[cnt].first)==0) p=find(cnt,parent,tp=0,rank),mp[v[cnt].first]=cnt;
-             else p=find(mp[v[cnt].first],parent,tp=0,rank),vis[cnt]=1;  // if already email comes we skip it by making it visited
-             cnt++;
-            
-             for(int i=2;i<vec.size();i++,cnt++){
-                 int t;
-                 if(mp.count(v[cnt].first)==0) t=find(cnt,parent,tp=0,rank),mp[v[cnt].first]=cnt;
-                 else t=find(mp[v[cnt].first],parent,tp=0,rank),vis[cnt]=1;
-                 if(t==p) continue;
-                 else if(rank[p]<rank[t]) parent[p]=t;
-                 else parent[t]=p;
-             }
+        DisjointSet ds(accounts.size());
+        unordered_map <string,vector <int>> emailToId;
+        for(int i=0;i<accounts.size();i++){
+            int id = i;
+            for(int j=1;j<accounts[i].size();j++){
+                string email = accounts[i][j];
+                if(emailToId.count(email)!=0){
+                    for(auto crntId:emailToId[email]){
+                        ds.findUnion(id,crntId);
+                    }
+                }
+                emailToId[email].push_back(id);
+            }
         }
-        vector<vector<string>>ans;
-        // print name of corresponding parent email
-        for(int i=0;i<parent.size();i++){
-            if(vis[i]) continue;
-            int p=find(i,parent,tp=0,rank);
-            string ofp=v[p].second;
-            vector<string>ss; ss.push_back(ofp);
-            vector<string>temp;
-             for(int j=0;j<parent.size();j++)
-                 if(!vis[j] and find(j,parent,tp=0,rank)==p)
-                     temp.push_back(v[j].first), vis[j]=1;
-            sort(temp.begin(),temp.end());
-            for(auto &str:temp) ss.push_back(str);
-            ans.push_back(ss);
+
+        vector <vector <string>> ans;
+        unordered_map <int,set <string>> idToEmail;
+
+        for(int i=0;i<accounts.size();i++){
+            int id = ds.getParent(i);
+            for(int j=1;j<accounts[i].size();j++){
+                idToEmail[id].insert(accounts[i][j]);
+            }
+        }
+        for(auto acc: idToEmail){
+            vector <string> temp;
+            temp.push_back(accounts[acc.first][0]);
+            for(auto name:acc.second)
+                temp.push_back(name);
+            ans.push_back(temp);
         }
         return ans;
     }
