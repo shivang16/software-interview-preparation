4cddf125947d4db0808f23c36abb964c8a0ce26b
Shivang <shivangmangal@gmail.com>
2023-09-05 18:36:19 +0200

    interviewPrep/Google/validParanthesisString.cpp MODIFIED

--- interviewPrep/Google/validParanthesisString.cpp
+++ interviewPrep/Google/validParanthesisString.cpp
@@ -1,29 +1,26 @@
+// https://leetcode.com/problems/valid-parenthesis-string/submissions/
 class Solution {
-    int dp[101][101];
-    bool solve(string &s, int index, int open){
-        if(index == s.size()){
-            return open == 0;
-        }
-        
-        if(dp[index][open] != -1){
-            return dp[index][open];
-        }
-        
-        if(s[index] == '('){
-            return dp[index][open] = solve(s, index + 1, open + 1);
-        }
-        else if(s[index] == ')'){
-            return dp[index][open] = open != 0 && solve(s, index + 1, open - 1);
-        }
-        else{
-            return dp[index][open] = solve(s, index + 1, open + 1) 
-                || open != 0 && solve(s, index + 1, open - 1)
-                || solve(s, index + 1, open);
-        }
-    }
+private: 
+    vector <vector <int>> dp;
 public:
+    bool checkValidity(string &s,int ind,int open){
+        if(ind==s.size()){
+            return open==0;
+        }
+        if(open<0) return false;
+        if(dp[ind][open]!=-1) return dp[ind][open];
+        bool ans;
+        if(s[ind]=='(')
+            ans =  checkValidity(s,ind+1,open+1);
+        else if(s[ind]==')')
+            ans = checkValidity(s,ind+1,open-1);
+        else{
+            ans = checkValidity(s,ind+1,open) || checkValidity(s,ind+1,open+1) || checkValidity(s,ind+1,open-1);
+        }
+        return dp[ind][open] = ans;
+    }
     bool checkValidString(string s) {
-        memset(dp, -1, sizeof(dp));
-        return solve(s, 0, 0);
+        dp.resize(105,vector <int> (105,-1));
+        return checkValidity(s,0,0);
     }
 };
