f9fc68f9dbc74cf840687aab79eb446f5d5f6bac
Shivang <shivangmangal@gmail.com>
2023-09-05 18:31:30 +0200

    interviewPrep/Google/numberOfFlowersInFullBloom.cpp MODIFIED

--- interviewPrep/Google/numberOfFlowersInFullBloom.cpp
+++ interviewPrep/Google/numberOfFlowersInFullBloom.cpp
@@ -1,30 +1,30 @@
+// https://leetcode.com/problems/number-of-flowers-in-full-bloom/submissions/
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        vector <int> ans;
-        map <int,int> M;
+        map <int,int> mp;
         for(auto i:flowers){
-            M[i[0]]++;
-            M[i[1]+1]--;
+            mp[i[0]]++;
+            mp[i[1]+1]--;
         }
-
-        int count = 0;
-        map <int,int> helper;
-        for(auto i:M){
-            count+=i.second;
-            helper[i.first] = count;
-            // cout << i.first << " " << i.second << " " << count << endl;
+        int sum = 0;
+        int mini = INT_MAX, maxi = INT_MIN;
+        for(auto &i:mp){
+            sum+=i.second;
+            i.second=sum;
+            mini = min(mini,i.first);
+            maxi = max(maxi,i.first);
         }
+        vector <int> ans;
         for(auto i:people){
-            auto ptr = helper.lower_bound(i);
-            // cout << ptr->first << endl;
-            if(ptr->first==i){
-                ans.push_back(ptr->second);
+            if(mp.count(i)){
+                ans.push_back(mp[i]);
             }else{
-                if(ptr==helper.begin())
-                    ans.push_back(0);
-                else
-                    ans.push_back(prev(ptr)->second);
+                if(i<mini or i>maxi) ans.push_back(0);
+                else{
+                    auto itr = --mp.lower_bound(i);
+                    ans.push_back(itr->second);
+                }
             }
         }
         return ans;
