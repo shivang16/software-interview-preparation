b2fc6d9dac1b9155bf41061f0216053eede33596
Shivang <shivangmangal@gmail.com>
2023-09-05 18:04:52 +0200

    interviewPrep/Google/combinations.cpp MODIFIED

--- interviewPrep/Google/combinations.cpp
+++ interviewPrep/Google/combinations.cpp
@@ -19,3 +19,23 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector <vector <int>> ans;
+        for(int i=0;i<(1<<n);i++){
+            if(__builtin_popcount(i)==k){
+                vector <int> temp;
+                int j = 0;
+                while(j<n){
+                    if(i & (1<<j)) temp.push_back(j+1);
+                    j++;
+                }
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};
